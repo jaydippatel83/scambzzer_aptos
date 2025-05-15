0a5f6b4a7fb5cd648832a63f4804391a8b91d43c
mansijoshi17 <mansijoshi1777@gmail.com>
2025-05-15 11:17:18 +0530

    Changes in auth route

--- src/app/auth/google/callback/route.ts
+++ src/app/auth/google/callback/route.ts
@@ -1,11 +1,10 @@
-import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const url = new URL(req.url);
   const code = url.searchParams.get("code");
 
-  redirect(
+  return NextResponse.redirect(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/google/callback?code=${code}`
   );
 };
